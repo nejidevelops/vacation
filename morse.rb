2c0e38c62c43f4afe139651b1a8e0d7bb8d5a2cc
Newton Ombese <newtonombese1@gmail.com>
2023-09-29 06:43:01 +0300

    Fix more linter errors

--- morse.rb
+++ morse.rb
@@ -8,17 +8,21 @@ class MorseCodeDecoder
     'Z' => '--..', '0' => '-----', '1' => '.----', '2' => '..---', '3' => '...--',
     '4' => '....-', '5' => '.....', '6' => '-....', '7' => '--...', '8' => '---..',
     '9' => '----.', ' ' => ' '
-  }
+  }.freeze
 
   def self.decode_char(morse_char)
     MORSE_CODE.key(morse_char)
   end
 
   def self.decode_word(morse_word)
-    morse_word.split(' ').map { |morse_char| decode_char(morse_char) }.join
+    morse_word.split.map { |morse_char| decode_char(morse_char) }.join
   end
 
   def self.decode_message(morse_message)
     morse_message.split('   ').map { |morse_word| decode_word(morse_word) }.join(' ')
   end
 end
+
+morse_message = ".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ..."
+decoded_message = MorseCodeDecoder.decode_message(morse_message)
+puts decoded_message
